97c27e23f599d4b7a5a61c1569e3086a8a85158a
Nicolas <6582485n@gmail.com>
2021-01-29 15:51:20 +0100

    lol

--- src/components/GlaconCard.js
+++ src/components/GlaconCard.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { Card, CardContent, Typography, Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { colors } from "../theme";
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { colors } from '../theme';
 
 const useStyles = makeStyles((theme) => ({
 	oneCard: {
-		backgroundColor: "#ECE9E9",
-		margin: "1vw",
+		backgroundColor: '#ECE9E9',
+		margin: '1vw',
 		width: 300,
 		height: 160,
 	},
 	cardContainer: {
-		padding: "2vh 8vw",
+		padding: '2vh 8vw',
 	},
 	title: {
-		fontWeight: "bold",
+		fontWeight: 'bold',
 		fontSize: 20,
 		color: colors.black,
 	},
 	description: {
-		fontStyle: "italic",
+		fontStyle: 'italic',
 		color: colors.grey,
 	},
 	priceAndQuantity: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingTop: "0px",
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingTop: '0px',
 	},
 	price: {
-		fontWeight: "bold",
+		fontWeight: 'bold',
 		color: colors.black,
 	},
 	quantity: { color: colors.grey },
@@ -38,28 +38,22 @@ const useStyles = makeStyles((theme) => ({
 const GlaconCards = ({ glacons }) => {
 	const styles = useStyles();
 
-	console.log("glaglacon", glacons);
+	console.log('glaglacon', glacons);
 
 	return (
 		<Grid container className={styles.cardContainer}>
-			{glacons["glacons"].map((glacon) => (
+			{glacons.map((glacon) => (
 				<Grid item>
 					<Card className={styles.oneCard}>
-						<div class="card-body">
+						<div class='card-body'>
 							<CardContent>
 								<Typography className={styles.title}>{glacon.name}</Typography>
-								<Typography className={styles.description}>
-									{glacon.description}
-								</Typography>
+								<Typography className={styles.description}>{glacon.description}</Typography>
 							</CardContent>
 
 							<CardContent className={styles.priceAndQuantity}>
-								<Typography className={styles.quantity}>
-									Quantité : {glacon.quantity}
-								</Typography>
-								<Typography className={styles.price}>
-									{glacon.price} €
-								</Typography>
+								<Typography className={styles.quantity}>Quantité : {glacon.quantity}</Typography>
+								<Typography className={styles.price}>{glacon.price} €</Typography>
 							</CardContent>
 						</div>
 					</Card>
