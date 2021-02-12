4072887b6df99f2edb9f6726335a231955e5e20b
Nicolas Lattemann <nicolas.lattemann@captaindata.co>
2021-02-12 15:48:27 +0100

    added route to product page

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
 
-import history from "./history";
-import Products from "./screens/Product/Products";
+import history from './history';
+import Products from './screens/Product/Products';
 // import Home from "./screens/HomePage/index";
-import Product from "./screens/ProductPage/index";
-import About from "./screens/About/About";
-import Contact from "./screens/Contact/Contact";
+import Product from './screens/ProductPage/index';
+import About from './screens/About/About';
+import Contact from './screens/Contact/Contact';
 
 export default class Routes extends Component {
-	render() {
-		return (
-			<Router history={history}>
-				<Switch>
-					{/* <Route path="/" exact component={Home} /> */}
-					<Route path="/Products" component={Products} />
-					<Route path="/Contact" component={Contact} />
-					<Route path="/About" component={About} />
-				</Switch>
-			</Router>
-		);
-	}
+    render() {
+        return (
+            <Router history={history}>
+                <Switch>
+                    {/* <Route path="/" exact component={Home} /> */}
+                    <Route path='/Products' component={Products} />
+                    <Route path='/Contact' component={Contact} />
+                    <Route path='/About' component={About} />
+                    <Route path='/Product' component={Product} />
+                </Switch>
+            </Router>
+        );
+    }
 }
