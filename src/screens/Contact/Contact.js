91926913fa3f52571f7d0b78e1c50db5ecc7ff13
Patricia De Carli <patriciadecarli87@gmail.com>
2021-03-05 14:17:58 +0100

    .

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -4,7 +4,8 @@ class Contact extends Component {
     render() {
         return (
             <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-                <div><h2>Contact Page</h2>
+                <div>
+                    <h2>Contact Page</h2>
                 </div>
             </div>
         );
