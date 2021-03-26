6f1b22390b1554566451ad7c648550f734225f2a
Nicolas Lattemann <nicolas.lattemann@captaindata.co>
2021-03-26 17:43:20 +0100

    where banana

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
-import Desert from "../image/desert.jpeg";
+import Desert from "../image/where_banana.png";
 
 const ErrorPage = () => {
     return (
         <Grid style={{ display: "flex", justifyContent: "center", fontWeight: "bold", flexDirection: "column" }}>
-            <Typography style={{ fontWeight: "bold", position: "absolute", left: "45%", bottom: "80%" }}>Error page</Typography>
+            {/* <Typography style={{ fontWeight: "bold", position: "absolute", left: "45%", bottom: "80%" }}>Error page</Typography> */}
             <img src={Desert} style={{ height: "100%", width: "100%" }} alt='Page not found' />
         </Grid>
     );
