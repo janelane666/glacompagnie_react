b77697c42850ae5ffc4037a8a4fedb12728f2c99
Nicolas Lattemann <nicolas.lattemann@captaindata.co>
2021-04-16 11:17:02 +0200

    création de la page dacceuil!!!

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { colors } from "../theme";
+import mountain from "../image/mountain.png";
+import { Button, Modal } from "react-bootstrap";
+
+const useStyles = makeStyles((theme) => ({
+    align: { display: "flex", justifyContent: "center", marginTop: "10%" },
+    name: { fontFamily: "Viga", display: "flex", justifyContent: "center" },
+    text: { fontFamily: "Karla", display: "flex", justifyContent: "center" },
+    button: { display: "flex", marginRight: "auto", marginLeft: "auto", textAlign: "center", width: "16%", fontFamily: "Karla" }
+}));
 
 const Home = () => {
+    const styles = useStyles();
+
     return (
-        <div style={{ display: "flex", justifyContent: "center", padding: 30 }}>
+        <div className={styles.align}>
             <div>
-                <h1>Home Page</h1>
+                <div>
+                    <h1 className={styles.name}>Restez au frais avec nous.</h1>
+                    <p className={styles.text}>Chez Glacompagnie nous valorisons les glaçons du monde entier. Connectons-nous.</p>
+                    <Button className={styles.button} href='/Products'>
+                        Achetez un Glaçon
+                    </Button>
+                </div>
+                <img className={styles.image} src={mountain} alt='Montagne de neige qui ressemble à un requin.'></img>
             </div>
         </div>
     );
--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import Products from "./ProductsList/ProductsList";
-// import Home from "./HomePage/index";
+import Home from "./HomePage/index";
 import Product from "./ProductPage/index";
 import About from "./About/About";
 import Contact from "./Contact/Contact";
@@ -15,7 +15,7 @@ export default class Routes extends Component {
             <Router history={history}>
                 <Switch>
                     <Route exact path='/' component={Products} />
-                    <Route path='/Home' component={Products} />
+                    <Route path='/Home' component={Home} />
                     <Route path='/Products' component={Products} />
                     <Route path='/Contact' component={Contact} />
                     <Route path='/About' component={About} />
